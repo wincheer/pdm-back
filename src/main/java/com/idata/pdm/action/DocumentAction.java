3b2b0bf5e745ff6211bb9c167d9c32ea3ca432c2
wincheer <wincheer@126.com>
2017-11-05 12:00:14 +0800

    后台文件上传

--- src/main/java/com/idata/pdm/action/DocumentAction.java
+++ src/main/java/com/idata/pdm/action/DocumentAction.java
@@ -1,7 +1,11 @@
 package com.idata.pdm.action;
 
+import java.io.File;
+import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,60 +16,60 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.idata.pdm.entity.Document;
 import com.idata.pdm.service.DocumentService;
 
 @RestController
 @CrossOrigin
-public class DocumentAction
-{
+public class DocumentAction {
+	private final static Logger logger = LoggerFactory.getLogger(DocumentAction.class);
+	
 	@Autowired
 	private DocumentService docService;
 	@Value("${upload_path}")
 	private String UPLOAD_PATH;
 
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
-	public void upload(@RequestParam MultipartFile file, @RequestParam Map<String, Object> data) throws Exception
-	{
+	public void upload(@RequestParam MultipartFile file, @RequestParam Map<String, Object> data) {
 
-		//doc - file - ver
-		//参数 file，docName,md5,projectId,employeeId
-		//		String originalFileName = file.getOriginalFilename();
-		//		String systemFileName = ""+System.currentTimeMillis();
-		//		File outputFile = new File(UPLOAD_PATH, file.getOriginalFilename());
-		//		file.transferTo(outputFile);
-
-		System.out.println(data.get("docName") + "------------------ true upload");
+		File outputFile = new File(UPLOAD_PATH, file.getOriginalFilename());
+		try {
+			file.transferTo(outputFile);
+			docService.upload(data);
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
 	}
 
 	@RequestMapping(value = "/quickUpload", method = RequestMethod.POST)
-	public Map<String, Object> quickUpload(@RequestBody Map<String, Object> data) throws Exception
-	{
+	public Map<String, Object> quickUpload(@RequestBody Map<String, Object> data) throws Exception {
 		System.out.println(data.get("docName") + "==== 检查文件MD5");
-		if (data.get("docName").equals("SzeliskiBook_20100903_draft.pdf"))
-		{
+		if (data.get("docName").equals("SzeliskiBook_20100903_draft.pdf")) {
 			System.out.println("不用上传");
-		}
-		else
-		{
+		} else {
 			System.out.println("需要上传");
 		}
 		return data;
 	}
 
 	@RequestMapping(value = "/checkMd5", method = RequestMethod.POST)
-	public void upload(@RequestParam Map<String, Object> data) throws Exception
-	{
+	public void upload(@RequestParam Map<String, Object> data) throws Exception {
 
 		// System.out.println(file.getOriginalFilename());
 		System.out.println(data.toString());
 	}
 
 	@RequestMapping(value = "/download", method = RequestMethod.GET)
-	public void download(@RequestParam int fileId) throws Exception
-	{
+	public void download(@RequestParam int fileId) throws Exception {
 
 		// System.out.println(file.getOriginalFilename());
 		System.out.println("File ID = " + fileId);
+	}
+	
+	@RequestMapping(value = "/docList", method = RequestMethod.GET)
+	public List<Document> selectDocumentList(@RequestParam int folderId) throws Exception {
+
+		return docService.selectFolderDocumentList(folderId);
 	}
 
 }
--- src/main/java/com/idata/pdm/service/DocumentService.java
+++ src/main/java/com/idata/pdm/service/DocumentService.java
@@ -1,5 +1,8 @@
 package com.idata.pdm.service;
 
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -9,45 +12,88 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.idata.pdm.common.Utils;
-import com.idata.pdm.dao.ActionLogMapper;
-import com.idata.pdm.entity.ActionLog;
+import com.idata.pdm.dao.DocumentMapper;
+import com.idata.pdm.dao.DocumentVersionMapper;
+import com.idata.pdm.dao.FileMapper;
+import com.idata.pdm.entity.Document;
+import com.idata.pdm.entity.DocumentVersion;
+import com.idata.pdm.entity.File;
 
 @Service
 @Transactional
-public class DocumentService
-{
+public class DocumentService {
 	private final static Logger logger = LoggerFactory.getLogger(DocumentService.class);
-	
+
 	@Autowired
-	private ActionLogMapper dao;
+	private DocumentMapper docDao;
+	@Autowired
+	private FileMapper fileDao;
+	@Autowired
+	private DocumentVersionMapper verDao;
 
+	/**
+	 * 用户上传文档： 1、查找md5对应的file,若没找到新建并返回fileId；若找到返回fileId
+	 * 2、查找当前项目下（要不要检查当前目录下？）同名文档，若有新建版本号，若无新建document，然后新建版本号: 3、新建doc_version
+	 * 
+	 * @param data，包含docName、docMd5、projectId、folderId、employeeId
+	 */
+	public void upload(Map<String, Object> data) {
 
-	public List<ActionLog> selectLogPageList(Map<String, Object> queryParam)
-	{
-		int pageNo = (Integer) queryParam.get("pageNo");
-		int pageSize = (Integer) queryParam.get("pageSize");
-		@SuppressWarnings("unchecked")
-		Map<String,Object> params =  (Map<String, Object>) queryParam.get("filter");
-		logger.info(params.toString());
-		
-		int start = (pageNo - 1) * pageSize;
-		params.put("start", start);
-		params.put("pageSize", pageSize);
-				
-		return dao.selectLogPageList(Utils.cleanMap(params));
+		logger.info("上传参数: " + data.toString());
+		String docName = data.get("docName").toString();
+		String docMd5 = data.get("docMd5").toString();
+		Integer projectId = Integer.parseInt(data.get("projectId").toString());
+		Integer folderId = Integer.parseInt(data.get("folderId").toString());
+		Integer employeeId = Integer.parseInt(data.get("employeeId").toString());
+
+		long timeMillis = System.currentTimeMillis();
+
+		File file = fileDao.selectFileByMd5(docMd5);
+		if (file == null) {
+			file = new File();
+			file.setFileName(timeMillis + "." + docName.split("\\.")[1]);
+			file.setFileMd5(data.get("docMd5").toString());
+			fileDao.insertFile(file);
+		}
+
+		Map<String, Object> queryParam = new LinkedHashMap<String, Object>();
+		queryParam.put("projectId", projectId);
+		//queryParam.put("folderId", folderId);
+		queryParam.put("documentName", docName);
+		queryParam.put("start", 0);
+		queryParam.put("pageSize", 1);
+
+		List<Document> docList = docDao.selectDocumentList(queryParam);
+		Document doc;
+		if (docList.size() != 0) {
+			doc = docList.get(0);
+		} else {
+			doc = new Document();
+			doc.setProjectId(projectId);
+			doc.setFolderId(folderId);
+			doc.setDocumentName(docName);
+			docDao.insertDocument(doc);
+		}
+
+		DocumentVersion ver = new DocumentVersion();
+		ver.setDocumentId(doc.getDocumentId());
+		ver.setFileId(file.getFileId());
+		ver.setUploadDate(new Date());
+		ver.setUploadEmployee(employeeId);
+		ver.setVersion(new Timestamp(timeMillis).toString());
+		verDao.insertDocumentVersion(ver);
+
 	}
 
+	public List<Document> selectFolderDocumentList(int folderId) {
+		
+		Map<String, Object> queryParam = new LinkedHashMap<String, Object>();
+		queryParam.put("folderId", folderId);
+		queryParam.put("start", 0);
+		queryParam.put("pageSize", Integer.MAX_VALUE);
 
-	public int totalLogs(Map<String, Object> queryParam)
-	{
-		int total = 0;
-		
-		@SuppressWarnings("unchecked")
-		Map<String,Object> params =  (Map<String, Object>) queryParam.get("filter");
-		total = dao.totalLogs(Utils.cleanMap(params));
-		
-		return total;
+		List<Document> docList = docDao.selectDocumentList(queryParam);
+		return docList;
 	}
-	
+
 }
