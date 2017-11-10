5140a4f0f892cc7ba59eeb0ca2f2e795b860e6c9
wincheer <wincheer@126.com>
2017-11-10 09:26:24 +0800

    同时兼容web方式

--- src/main/java/com/idata/pdm/PdmApplication.java
+++ src/main/java/com/idata/pdm/PdmApplication.java
@@ -3,11 +3,18 @@ package com.idata.pdm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan(basePackages="com.idata.pdm.dao")
-public class PdmApplication
+public class PdmApplication extends SpringBootServletInitializer
 {
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PdmApplication.class);
+    }
+	
 	public static void main(String[] args)
 	{
 		SpringApplication.run(PdmApplication.class, args);
